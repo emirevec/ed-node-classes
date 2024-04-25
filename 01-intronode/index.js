e85330e30251b1ab65bdcfadbb8e367781c7d2e6
emirevec <vecchioemiliano@gmail.com>
2024-04-25 15:56:09 -0300

    feat: create first server o index.js and change type to module into pacage.json

--- 01-intronode/index.js
+++ 01-intronode/index.js
@@ -1,1 +1,29 @@
-console.log('Hello world from Node JS')
+// server.mjs
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  const url = req.url
+
+  console.log(req.url)
+  console.log(req.method)
+  console.log(req.headers)
+
+  switch (url) {
+    case '/':
+      res.end('Hello world!\n')
+      break
+    case '/products':
+      res.end('Products are...\n')
+      break
+    default:
+      res.end('Error 404\n')
+  }
+});
+
+// starts a simple http server locally on port 8080
+server.listen(8080, '127.0.0.1', () => {
+  console.log('Listening on http://localhost:8080/');
+});
+
+// run with `node server.mjs`
