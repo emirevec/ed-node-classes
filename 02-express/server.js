75294bf366e8dba27226bcaf4d9575a70c771833
emirevec <vecchioemiliano@gmail.com>
2024-04-26 10:48:57 -0300

    feat(server): add new path responses

--- 02-express/server.js
+++ 02-express/server.js
@@ -2,12 +2,46 @@ const express = require('express')
 
 const server = express()
 
-const port = 9000
+const PORT = 9000
 
 server.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('My first back-end!')
 })
 
-server.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+server.get('/json', (req, res) => {
+  try {
+    return res.status(200).json({
+      res: 'I am the json response'
+    })
+  } catch {
+    return res.status(404).json({
+      res: 'Error, file not found'
+    })
+  }
+})
+
+server.get('/html', (req, res) => {
+  res.status(200).end(`
+    <h1 style="color: blue">Welcome to our first app!!"</h1>
+  `)
+})
+
+server.get('/file', (req, res) => {
+  res.status(200).sendFile(__dirname + '/index.html')
+})
+
+server.post('/file', (req, res) => {
+  res.send('Succed')
+})
+
+server.get('/download', (req, res) => {
+  res.status(200).download(__dirname + '/invoice.pdf')
+})
+
+server.get('/goto', (req, res) => {
+  res.status(200).redirect('http://www.google.com')
+})
+
+server.listen(PORT, () => {
+  console.log(`Example app listening on PORT http://localhost:${PORT}`)
 })
