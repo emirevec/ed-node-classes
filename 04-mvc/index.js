d3a890b0a7bc34b218f4c17f8687f23ebdf3f31d
emirevec <vecchioemiliano@gmail.com>
2024-05-16 12:01:31 -0300

    build: add 'morgan' into index.js

--- 04-mvc/index.js
+++ 04-mvc/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const userRouter = require('./router/userRouter')
+const morgan = require('morgan')
+//const hbs = require('hbs')
+const userRouter = require('./routers/userRouter')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
