4ae346ddcce86dff8d2dbf77b96940ce6465e899
emirevec <vecchioemiliano@gmail.com>
2024-05-24 12:00:04 -0300

    fix: correct set of userControllers

--- 04-mvc/controllers/userControllers.js
+++ 04-mvc/controllers/userControllers.js
@@ -1,14 +1,34 @@
 const { request, response } = require('express')
+const User = require('../models/userModels')
 
 const getUsers = (req = request, res = response) => {
   res.send(`<h1>User<h1/>`)
 }
 
-const createUsers = (req = request, res = response) => {
-  const user = req.body
+const createUsers = async (req = request, res = response) => {
+  const user = new User(req.body)
+  const userSaved = await user.save()
+  if (userSaved) {
+    return res.render('resForm', {
+      email: req.body.email,
+      password: req.body.password,
+    })
+  }
+  
+  /* 
+  try {
+    user.save()
+  } catch (err) {
+    throw new Error('Error' + err)
+    return res.send('error', {error: err})
+  }
   res.json({
     users: 'usuario'
-  })
+  }) */
+}
+
+const getLogIn = (req = request, res = response) => {
+
 }
 
 const updateUsers = (req = request, res = response) => {
@@ -25,6 +45,7 @@ const deleteUsers = (req = request, res = response) => {
 
 module.exports = {
   getUsers,
+  getLogIn,
   createUsers,
   updateUsers,
   deleteUsers
