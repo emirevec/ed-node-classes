d44b1c92467969856eed2cf1f4af6d9a18e7a9b2
emirevec <vecchioemiliano@gmail.com>
2024-05-31 12:09:54 -0300

    feat: add request log middleware

--- 04-mvc/routers/userRouter.js
+++ 04-mvc/routers/userRouter.js
@@ -1,5 +1,4 @@
-const express = require('express')
-const router = express.Router()
+const router = require('express').Router()
 const { body, check } = require('express-validator')
 const { 
   getUsers,
