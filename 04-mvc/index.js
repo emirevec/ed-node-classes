5e68c4b4ed307198cef66071699d444dce3b3c51
emirevec <vecchioemiliano@gmail.com>
2024-07-30 17:01:53 -0300

    docs: add jsdoc comments on index and server

--- 04-mvc/index.js
+++ 04-mvc/index.js
@@ -1,3 +1,7 @@
+/**
+ * Main application file.
+ * @module app
+ */
 import express from 'express'
 import morgan from 'morgan'
 import hbs from 'hbs'
@@ -5,8 +9,11 @@ import { productRouter, userRouter } from './routers/index.js'
 import cookieParser from 'cookie-parser'
 //const addRequestLog = require('./middlewares')
 
+/** 
+ * Middleware and Configurations
+ * ==============================
+*/
 const app = express()
-
 //app.use(addRequestLog)
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -14,16 +21,29 @@ app.use(express.static('public'))
 app.use(cookieParser())
 app.use(morgan('dev'))
 
+/**
+ * View Engine Setup
+ * =================
+ */
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 hbs.registerPartials('views/partials')
 
+/**
+ * Route Handlers
+ * ==============
+ */
 app.use('/user', userRouter)
 app.use('/product', productRouter)
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+/**
+ * Error route.
+ * Renders the error view for any undefined routes.
+ * @route GET *
+ */
 app.get('*', (req, res) => {
   res.render('error')
 })
--- 04-mvc/server.js
+++ 04-mvc/server.js
@@ -1,11 +1,24 @@
+/**
+ * @fileoverview Main server file.
+ * Initializes environment variables, sets up the database connection, and starts the Express server.
+ */
 import dotenv from 'dotenv'
 import app from './index.js'
 import connection from './database/connection.js'
 
+// Load environment variables from .env file.
 dotenv.config()
+
+/**
+ * The port number on which the server listens.
+ * @const {number|string}
+ */
 const PORT = process.env.PORT || 9000
 
-
+/**
+ * Starts the server and listens on the specified port.
+ * Logs a message indicating the server is running and the URL where it's accessible.
+ */
 app.listen(PORT, () => {
   console.log(`Example app listening on PORT http://localhost:${PORT}`)
 })
