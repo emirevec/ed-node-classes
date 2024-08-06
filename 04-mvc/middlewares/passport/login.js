9484f97fb0cf8c96ee19d9dace59dedb91950030
emirevec <vecchioemiliano@gmail.com>
2024-08-06 10:39:23 -0300

    feat: add passport-local login strategy

--- 04-mvc/middlewares/passport/login.js
+++ 04-mvc/middlewares/passport/login.js
@@ -3,48 +3,35 @@ import bcrypt from 'bcrypt'
 import passport from 'passport-local'
 const LocalStrategy = passport.Strategy
 
-
-// --------------------
-
-module.exports = function(passport){
-  // passport/login.js
+function logIn(passport) {
   passport.use('login', new LocalStrategy(
     {
-    passReqToCallback: true
-    }, 
+      passReqToCallback: true
+    },
     function (req, username, password, done) {
-    // check in mongo if a user with username exists or not
+      // check in mongo if a user with username exists or not
       User.findOne({ 'username': username },
-      function (err, user) {
-        // In case of any error, return using the done method
-        if (err) return done(err);
-        // Username does not exist, log error & redirect back
-        if (!user) {
-        console.log('User Not Found with username ' + username);
-        return done(null, false, req.flash('message', 'User Not found.'));
-        }
-        // User exists but wrong password, log the error
-        if (!isValidPassword(user, password)) {
-        console.log('Invalid Password');
-        return done(null, false, req.flash('message', 'Invalid Password'));
-        }
-        // User and password both match, return user from
-        // done method which will be treated like success
-        return done(null, user);
-      }
-    );
-    }));
-    var isValidPassword = function(user, password){
-      return bCrypt.compareSync(password, user.password);
-      }
-      }
+        function (err, user) {
+          // In case of any error, return using the done method
+          if (err) return done(err);
+          // Username does not exist, log error & redirect back
+          if (!user) {
+            console.log('User Not Found with username ' + username);
+            return done(null, false, req.flash('message', 'User Not found.'));
+          }
+          // User exists but wrong password, log the error
+          if (!isValidPassword(user, password)) {
+            console.log('Invalid Password');
+            return done(null, false, req.flash('message', 'Invalid Password'));
+          }
+          // User and password both match, return user from
+          // done method which will be treated like success
+          return done(null, user);
+        })
+    }))
+  var isValidPassword = function (user, password) {
+    return bcrypt.compareSync(password, user.password);
+  }
+}
 
-      passport.serializeUser(function (user, done) {
-        done(null, user._id);
-        });
-        var User = require('./models/user.js');
-        passport.deserializeUser(function (id, done) {
-        User.findById(id, function (err, user) {
-        done(err, user);
-        });
-        });
+export default logIn
