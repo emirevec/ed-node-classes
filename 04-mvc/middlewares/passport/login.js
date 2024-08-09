7a1747c8d3db5c521e4e13f8887830343682f502
emirevec <vecchioemiliano@gmail.com>
2024-08-09 10:48:57 -0300

    fix: change LocalStrategy cb to async function

--- 04-mvc/middlewares/passport/login.js
+++ 04-mvc/middlewares/passport/login.js
@@ -5,32 +5,30 @@ import { Strategy as LocalStrategy } from 'passport-local'
 export default function(passport) {
   passport.use('login', new LocalStrategy(
     {
+      usernameField: 'email',
+      passwordField: 'password',
       passReqToCallback: true
     },
-    function (req, username, password, done) {
-      // check in mongo if a user with username exists or not
-      User.findOne({ 'username': username },
-        function (err, user) {
-          // In case of any error, return using the done method
-          if (err) return done(err);
-          // Username does not exist, log error & redirect back
-          if (!user) {
-            console.log('User Not Found with username ' + username);
-            return done(null, false, { message: 'User not found.' })
-          }
-          // User exists but wrong password, log the error
-          if (!isValidPassword(user, password)) {
-            console.log('Invalid Password');
-            return done(null, false, { message: 'Invalid Password' });
-          }
-          // User and password both match, return user from
-          // done method which will be treated like success
-          return done(null, user);
-        })
+    async function (req, email, password, done) {
+      try {
+        const user = await User.findOne({ 'email': email })
+        if (!user) {
+          console.log('User not found.')
+          return done(null, false, { message: 'User not found.' })
+        }
+        if (!isValidPassword(user, password)) {
+          console.log('Invalid Password')
+          return done(null, false, { message: 'Invalid Password' })
+        }
+          return done(null, user)
+      } catch (error) {
+        console.log('User or password wrong' + email)
+        return done(error)
+      }
     }))
-  var isValidPassword = function (user, password) {
-    return bcrypt.compareSync(password, user.password);
-  }
+    const isValidPassword = function (user, password) {
+      return bcrypt.compareSync(password, user.password)
+    } 
   return passport
 }
 
