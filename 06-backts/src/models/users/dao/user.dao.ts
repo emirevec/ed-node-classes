24cbc55dfa5f368eede2bf50cf892c38b6ddd4ce
emirevec <vecchioemiliano@gmail.com>
2024-08-16 12:16:20 -0300

    fix: correct user.dao import

--- 06-backts/src/models/users/dao/user.dao.ts
+++ 06-backts/src/models/users/dao/user.dao.ts
@@ -1,7 +1,7 @@
 import { CreateUserDto } from "../dto/create.user.dto"
 import { PatchUserDto } from "../dto/patch.user.dto"
 import { PutUserDto } from "../dto/put.user.dto"
-import nanoid from "nanoid"
+import { nanoid } from "nanoid"
 import debug from "debug"
 
 const log: debug.IDebugger = debug('app:in-memory-dao')
--- 06-backts/src/models/users/dto/patch.user.dto.ts
+++ 06-backts/src/models/users/dto/patch.user.dto.ts
@@ -1,3 +1,3 @@
-import { PutUserDto } from './put.user.dto.ts'
+import { PutUserDto } from './put.user.dto'
 
 export interface PatchUserDto extends Partial<PutUserDto> {}
